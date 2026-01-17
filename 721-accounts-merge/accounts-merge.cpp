9b292b70fcf0411f1a35cc0fc3e4ea8e267e5e80
Dhruv Gupta <164864277+DhruvGupta6177@users.noreply.github.com>
2026-01-17 16:11:18 +0530

    Time: 28 ms (76.98%) | Memory: 40.5 MB (51.93%) - LeetSync

--- 721-accounts-merge/accounts-merge.cpp
+++ 721-accounts-merge/accounts-merge.cpp
@@ -1,59 +1,84 @@
+class DisjointSet {
+    vector<int> rank, parent, size;
+
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1, 1);
+
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUpar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUpar(parent[node]);
+    }
+
+    void UnionBySize(int u, int v) {
+        int ulp_u = findUpar(u);
+        int ulp_v = findUpar(v);
+
+        if (ulp_u == ulp_v)
+            return;
+
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
 class Solution {
 public:
-    vector<int> parent;
-
-    int find(int x) {
-        if (parent[x] != x)
-            parent[x] = find(parent[x]);
-        return parent[x];
-    }
-
-    void unite(int x, int y) {
-        int px = find(x);
-        int py = find(y);
-        if (px != py)
-            parent[py] = px;
-    }
-
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
         int n = accounts.size();
-        parent.resize(n);
+        DisjointSet ds(n);
+        unordered_map<string, int> mapMailNode;
 
-        for (int i = 0; i < n; i++)
-            parent[i] = i;
-
-        unordered_map<string, int> emailToAccount;
-
-        // Step 1: Union accounts sharing emails
+        // Step 1: Union accounts by common email
         for (int i = 0; i < n; i++) {
             for (int j = 1; j < accounts[i].size(); j++) {
-                string email = accounts[i][j];
-                if (emailToAccount.count(email)) {
-                    unite(i, emailToAccount[email]);
+                string mail = accounts[i][j];
+                if (mapMailNode.find(mail) == mapMailNode.end()) {
+                    mapMailNode[mail] = i;
                 } else {
-                    emailToAccount[email] = i;
+                    ds.UnionBySize(i, mapMailNode[mail]);
                 }
             }
         }
 
-        // Step 2: Group emails by root account
-        unordered_map<int, set<string>> groups;
-
-        for (auto &it : emailToAccount) {
-            int root = find(it.second);
-            groups[root].insert(it.first);
+        // Step 2: Group emails by parent
+        vector<vector<string>> mergedMail(n);
+        for (auto it : mapMailNode) {
+            string mail = it.first;
+            int node = ds.findUpar(it.second);
+            mergedMail[node].push_back(mail);
         }
 
-        // Step 3: Build result
-        vector<vector<string>> result;
-        for (auto &g : groups) {
-            vector<string> account;
-            account.push_back(accounts[g.first][0]); // name
-            for (auto &email : g.second)
-                account.push_back(email);
-            result.push_back(account);
+        // Step 3: Build answer
+        vector<vector<string>> ans;
+
+        for (int i = 0; i < n; i++) {
+            if (mergedMail[i].empty())
+                continue;
+
+            sort(mergedMail[i].begin(), mergedMail[i].end());
+
+            vector<string> temp;
+            temp.push_back(accounts[i][0]); // name
+            for (auto &mail : mergedMail[i]) {
+                temp.push_back(mail);
+            }
+            ans.push_back(temp);
         }
 
-        return result;
+        return ans;
     }
 };
