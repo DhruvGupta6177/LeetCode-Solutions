601568c64d30ce181bd77186024e92fa45e07257
Dhruv Gupta <164864277+DhruvGupta6177@users.noreply.github.com>
2026-01-29 12:30:09 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (78.41%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        if (matrix.empty()) return;
         int n = matrix.size();
         int m = matrix[0].size();
 
-        // Determine if first row or first col initially contain zero
-        bool firstRowZero = false, firstColZero = false;
-        for (int j = 0; j < m; ++j) if (matrix[0][j] == 0) { firstRowZero = true; break; }
-        for (int i = 0; i < n; ++i) if (matrix[i][0] == 0) { firstColZero = true; break; }
+        vector<int>col(m,0);
+        vector<int>row(n,0);
 
-        // Use first row/col as markers for other rows/cols
-        for (int i = 1; i < n; ++i) {
-            for (int j = 1; j < m; ++j) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+        for(int i = 0; i <n; i++){
+            for(int j = 0;j < m; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
 
-        // Zero cells based on markers
-        for (int i = 1; i < n; ++i) {
-            for (int j = 1; j < m; ++j) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+        for(int i = 0; i <n; i++){
+            for(int j = 0;j < m; j++){
+                if(row[i] || col[j]){
                     matrix[i][j] = 0;
                 }
+
             }
         }
-
-        // Zero first row/col if needed
-        if (firstRowZero) {
-            for (int j = 0; j < m; ++j) matrix[0][j] = 0;
-        }
-        if (firstColZero) {
-            for (int i = 0; i < n; ++i) matrix[i][0] = 0;
-        }
+        
     }
 };
