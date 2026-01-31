a7f416b938163d47fda37dc113239ebbd4df81bd
Dhruv Gupta <164864277+DhruvGupta6177@users.noreply.github.com>
2026-01-31 23:46:40 +0530

    Time: 49 ms (53.67%) | Memory: 29 MB (90.09%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;    // SC = O(UniqueTriplets)
+
+        sort(nums.begin(), nums.end());         // TC = O(nLog(n))
+
+        for(int i = 0; i < n; i++){             // TC = O(n)
+
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+
+            int j = i + 1, k = n - 1;
+            
+            while(j < k){                       // TC = O(n)
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if(sum > 0){
+                    k--;
+                }
+                else if(sum < 0){
+                    j++;
+                }
+                else{
+                    ans.push_back({nums[i] ,nums[j] ,nums[k]});
+                    j++; k--;
+
+                    while(j < k && nums[j] == nums[j-1]){
+                        j++;
+                    }
+                }
+            }
+
+            
+        }
+
+        return ans;
+    }
+};
